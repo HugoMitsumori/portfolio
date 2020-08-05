9dfab924069d4b7a26da8a4d5181b8a3845c76af
Hugo Mitsumori <hugo.mitsumori@avante.com.vc>
2020-08-05 10:34:15 -0300

    Update dist files

---
+++ dist/precache-manifest.587fa7f5a797ee137502fbe71aa96b72.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec73b33488a24afe72da",
+    "url": "/portfolio/css/app.b08fc969.css"
+  },
+  {
+    "revision": "5eb7fcb8be006ff8eba8",
+    "url": "/portfolio/css/chunk-vendors.7d698daa.css"
+  },
+  {
+    "revision": "a930bee1a3671c47ca1c",
+    "url": "/portfolio/css/home.b4d97355.css"
+  },
+  {
+    "revision": "122ad02f27de773ec3dd0af297fc3580",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9fd638eaf57f079b2d5b",
+    "url": "/portfolio/js/about.e242f633.js"
+  },
+  {
+    "revision": "ec73b33488a24afe72da",
+    "url": "/portfolio/js/app.3ab7edf1.js"
+  },
+  {
+    "revision": "5eb7fcb8be006ff8eba8",
+    "url": "/portfolio/js/chunk-vendors.f5ba5e5a.js"
+  },
+  {
+    "revision": "a930bee1a3671c47ca1c",
+    "url": "/portfolio/js/home.2b06e90c.js"
+  },
+  {
+    "revision": "fda789b58d696bb5b27995d6ddce2989",
+    "url": "/portfolio/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/portfolio/robots.txt"
+  }
+]);
